9dc0395a41a8d8ff5abdc6097ec4d71b746218a9
Matheus <math3226@gmail.com>
2023-03-26 21:18:11 -0300

    [feat] Lançando eventos de domínio

---
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using NerdStore.Catalogo.Domain.Interfaces;
+
+namespace NerdStore.Catalogo.Domain.Events
+{
+    public class ProdutoEventHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoEventHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task Handle(ProdutoAbaixoEstoqueEvent notification, CancellationToken cancellationToken)
+        {
+            var produto = await _produtoRepository.ObterPorId(notification.AggregateId);
+
+            // Possíveis TODOs
+        }
+    }
+}
