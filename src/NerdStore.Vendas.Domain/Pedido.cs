36823d36f485f86fcece4356918b6bec5213c9ab
Matheus de Lima Santana <math3226@gmail.com>
2023-04-11 20:44:56 -0300

    [feat] Setup do BC de Vendas

---
+++ src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,10 @@
+﻿using NerdStore.Core.DomainObjects;
+using NerdStore.Core.DomainObjects.Interfaces;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido : Entity, IAggregateRoot
+    {
+
+    }
+}
